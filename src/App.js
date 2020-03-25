0d621c5c79e09ffd5263882f435d2311f218fbfd
youssef mahersi <youssefmahersi0@gmail.com>
2020-03-26 00:37:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactGa from "react-ga";
 import Layout from "./Layout";
+import AdSense from 'react-adsense';
 function App() {
   useEffect(()=>{
     ReactGa.initialize("UA-161446282-1");
@@ -12,6 +13,13 @@ function App() {
   return (
     <div className="App">
    <Layout/>
+   <AdSense.Google
+  client='ca-pub-5666725616144116'
+  slot='7806394673'
+  style={{ display: 'block' }}
+  layout='in-article'
+  format='fluid'
+/>
     </div>
   );
 }
