3b020af2413e5356aff24dd87234f32f57660db7
youssef mahersi <youssefmahersi0@gmail.com>
2020-03-28 03:20:47 +0100

    dddd

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -8,6 +8,7 @@ class Application extends React.Component {
             <Helmet>
                 <meta charSet="utf-8" />
                 <title>Anl-virus</title>
+                <meta name="msvalidate.01" content="AE72042242E3FA7FF419652FD3C545C0" />
                 <meta name="keywords" content="oms,who,coronavirus,covid-19,anal-virus,corona tracker,anal-vir"></meta>
                 <meta name="description" content="tracks all the cases of coronavirus(recovered,confirmed,deaths)" />
                 <link rel="canonical" href="https://anal-virus.info" />
